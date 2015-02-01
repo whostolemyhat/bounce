1251c5dc9b3c5ed37b87d5c04dda6369e4c06eaa
James Tease <jamestease@gmail.com>
2015-02-01 21:04:22 +0000

    movement

--- app/js/play.js
+++ app/js/play.js
@@ -3,7 +3,22 @@ var platform = platform || {};
 
 platform.playState = {
     create: function() {
-        this.GRAVITY = 200;
+        this.GRAVITY = 1800;
+        this.PLAYER_SPEED = 200;
+        this.JUMP_SPEED = -500;
+        this.maxJumpTime = 150;
+        this.onGround = false;
+        this.jumping = false;
+        this.jumpTime = 0;
+        this.jumpStart = 0;
+
+        this.cursor = this.game.input.keyboard.createCursorKeys();
+        game.input.keyboard.addKeyCapture([
+            Phaser.Keyboard.UP,
+            Phaser.Keyboard.DOWN,
+            Phaser.Keyboard.LEFT,
+            Phaser.Keyboard.RIGHT
+        ]);
 
         game.stage.backgroundColor = '#21a5e0';
 
@@ -16,12 +31,69 @@ platform.playState = {
         game.camera.follow(this.player, Phaser.Camera.FOLLOW_PLATFORMER);
 
         this.loadMap();
-
+        console.log(this.layer);
+        console.log(this.lava);
     },
 
     update: function() {
         game.physics.arcade.collide(this.layer, this.player);
+        this.onGround = this.player.body.onFloor();
+        this.movePlayer();
+        game.physics.arcade.collide(this.layer, this.player);
+        game.physics.arcade.overlap(this.lava, this.player, this.playerDie, null, this);
         game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
+
+        if(!this.player.inWorld) {
+            this.playerDie();
+        }
+    },
+
+    movePlayer: function() {
+        if(this.cursor.left.isDown) {
+            this.player.body.velocity.x = - this.PLAYER_SPEED;
+        } else if(this.cursor.right.isDown) {
+            this.player.body.velocity.x = this.PLAYER_SPEED;
+        } else {
+            this.player.body.velocity.x = 0;
+        }
+
+        // console.log(this.cursor.up.isDown, this.onGround, !this.jumping);
+        // jump
+        if(this.cursor.up.isDown && this.onGround && !this.jumping) {
+            console.log('jump');
+
+            // jumping from ground
+            this.jumpTime = 0;
+            this.onGround = false;
+            this.jumping = true;
+            this.jumpStart = game.time.now;
+
+            this.player.body.velocity.y = this.JUMP_SPEED;
+        } else if(this.cursor.up.isDown &&
+                  !this.onGround &&
+                  this.jumpTime < this.maxJumpTime &&
+                  this.jumping) {
+            console.log('still jumping');
+            // carry on jumping
+            this.jumpTime = game.time.elapsedSince(this.jumpStart);
+
+            // this.player.body.velocity.y = this.JUMP_SPEED;
+        } else if(!this.onGround &&
+                  (!this.cursor.up.isDown || this.jumpTime >= this.maxJumpTime)) {
+            // falling
+            console.log('falling');
+            this.jumping = false;
+        }
+
+        // jump released, reset 
+        if(!this.cursor.up.isDown && this.onGround) {
+            this.jumping = false;
+            this.jumpTime = 0;
+        }
+    },
+
+    playerDie: function() {
+        console.log('hit lava');
     },
 
     loadMap: function() {
@@ -29,12 +101,14 @@ platform.playState = {
         this.map.addTilesetImage('tileset');
         this.layer = this.map.createLayer('walls');
         this.layer.resizeWorld();
-
-        this.lava = this.map.createLayer('lava');
         this.map.setCollision(1);
 
+        this.lava = this.map.createLayer('lava');
+        this.map.setCollision(3, true, 'lava');
+        
         // player
         // this.map.createFromObjects('player', 5, 'player', 0, true, false, this.player);
+       
         // coins
         this.map.createFromObjects('coins', 4, 'coin', 0, true, false, this.coins);
     },
